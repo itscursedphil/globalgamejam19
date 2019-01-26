ba8530825f64c7e4523a0dbdd4e7574ac47cc7fd
Martin Gawlita <martin.gawlita@hd-automation.com>
2019-01-26 14:36:24 +0100

    background

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
 import { Graphics } from './graphics';
 import { RenderedItem } from './renderedItem';
 import { Layer } from './layer';
+import { Background } from './backgound';
 
-const graphics = new Graphics([new Layer([new RenderedItem()])]);
+window.addEventListener("load", () => {
+  const graphics = 
+    new Graphics([new Layer([new Background('./assets/bg_space.png')])]);
+});
 
-console.log(graphics.canvas.height);
+
