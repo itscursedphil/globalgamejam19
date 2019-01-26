1dceedac08d815137ec84ca98b66875c2dc3928c
BratPfanneTV <39345937+BratPfanneTV@users.noreply.github.com>
2019-01-26 22:54:56 +0100

    Speed Updates 2.0

--- src/js/State/State.js
+++ src/js/State/State.js
@@ -1,33 +1,36 @@
+import spriteFile from '../../assets/images/spr_assets_128.png';
+import ResourceSpawner from '../resourceSpawner';
+import SpriteSheet from '../engine/spriteSheet';
+import Sprite from '../engine/sprite';
 import Inventory from './inventory';
 import Player from './player';
 import EnvItem from './envItem';
-import SpriteSheet from './../engine/spriteSheet';
-import Sprite from './../engine/sprite';
-import spriteFile from '../../assets/images/spr_assets_128.png'
 
 export default class State {
-	constructor() {
-		this.inventory = new Inventory();
+  constructor() {
+    this.inventory = new Inventory();
     this.envitems = [];
-    this.spriteSheet = 
-      new SpriteSheet(spriteFile, 128, 128);
+    this.spriteSheet = new SpriteSheet(spriteFile, 128, 128);
     this.spriteSheet.load().then(() => {
       this.player = new Player(this.spriteSheet);
+      this.rs = new ResourceSpawner(this.player, this);
     });
-	}
+  }
 
-	update(availableHeight, availableWidth) {
-		this.gameTick(availableHeight, availableWidth);
-	}
+  update(availableHeight, availableWidth) {
+    this.gameTick(availableHeight, availableWidth);
+  }
 
-	render(ctx) {
-    if(this.player)
+  render(ctx) {
+    if (this.player) {
       this.player.render(ctx);
-	}
+    }
+  }
 
-	gameTick(availableHeight, availableWidth, tps = 60) {    
+  gameTick(availableHeight, availableWidth, tps = 60) {
     this.inventory.gameTick(tps);
-    if(this.player)
-		  this.player.gameTick(availableHeight, availableWidth, tps);
-	}
+    if (this.player) {
+      this.player.gameTick(availableHeight, availableWidth, tps);
+    }
+  }
 }
