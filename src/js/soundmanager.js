4e33d64a41c3ae37882aa9682236082d73efb951
BratPfanneTV <39345937+BratPfanneTV@users.noreply.github.com>
2019-01-26 12:19:42 +0100

    Soundmanager 2.0

--- src/js/soundmanager.js
+++ src/js/soundmanager.js
@@ -1,89 +1,62 @@
-export default class ManagedSound {
-	constructor(url, volume) {
-		this.url = url;
-		this.maxvol = volume;
-		this.loopcount = -1;
-		this.loop = false;
-		this.onendfunc = function () { };
+import Sound from './sound';
+
+export default class SoundManager {
+	constructor() {
+		this.runningSounds = [];
+		this.interval = setInterval(() => this.checkRunning(), 10);
 	}
 
-	load() {
-		this.audio = new Audio(this.url);
-		const t = this;
-		this.audio.addEventListener('ended', function () {
-			if (t.loopcount !== 1 && t.loop) {
-				this.currentTime = 0;
-				this.play();
+	checkRunning() {
+		this.runningSounds.forEach((e, i) => this.checkRunningFor(e, i));
+	}
+
+	checkRunningFor(element = new Sound(), index = 0, array = []) {
+		if (!element.playing()) {
+			array.splice(index);
+		}
+	}
+
+	addLayer(url = '', volume = 1) {
+		const newobj = new Sound(url, volume);
+		this.runningSounds.push(newobj);
+		return newobj;
+	}
+
+	pauseAll() {
+		this.runningSounds.forEach(element => {
+			element.pause();
+		});
+	}
+
+	playAll() {
+		this.runningSounds.forEach(element => {
+			element.play();
+		});
+	}
+
+	stopAll() {
+		this.runningSounds.forEach(element => {
+			element.stop();
+		});
+	}
+
+	fadeOutAll(seconds = 0) {
+		this.runningSounds.forEach(element => {
+			element.fadeOut(seconds);
+		});
+	}
+
+	fadeInAll(seconds = 0) {
+		this.runningSounds.forEach(element => {
+			element.fadeIn(seconds);
+		});
+	}
+
+	loadAll() {
+		this.runningSounds.forEach(element => {
+			if (!element.loaded) {
+				element.load();
 			}
-
-			t.loopcount--;
-			t.onendfunc();
-		}, false);
-	}
-
-	play() {
-		this.audio.play();
-		this.checkforEnd = true;
-	}
-
-	onEnd(func) {
-		this.onendfunc = func;
-	}
-
-	playing() {
-		return this.audio &&
-            this.audio.currentTime > 0 &&
-            !this.audio.paused &&
-            !this.audio.ended &&
-            this.audio.readyState > 2;
-	}
-
-	pause() {
-		this.audio.pause();
-	}
-
-	stop() {
-		this.audio.pause();
-		this.audio.currentTime = 0;
-	}
-
-	fadeIn(seconds) {
-		const t = this;
-		this.audio.volume = 0;
-		const setup = (this.maxvol - this.audio.volume) / seconds / 100;
-		this.play();
-		const fadeininterval = setInterval(() => {
-			if (t.audio.volume + setup < 1.0) {
-				t.audio.volume += setup;
-			} else {
-				clearInterval(fadeininterval);
-			}
-		}, 10);
-	}
-
-	fadeOut(seconds) {
-		const t = this;
-		this.audio.volume = this.maxvol;
-		const setdown = this.audio.volume / seconds / 100;
-		const fadeoutinterval = setInterval(() => {
-			if (t.audio.volume > 0.0) {
-				if (t.audio.volume - setdown < 0) {
-					t.audio.volume = 0;
-				} else {
-					t.audio.volume -= setdown;
-				}
-			} else {
-				clearInterval(fadeoutinterval);
-			}
-		}, 10);
-	}
-
-	loop(bool) {
-		this.loop = bool;
-	}
-
-	loopTimes(times) {
-		this.loop = true;
-		this.loopcount = times;
+		});
 	}
 }
