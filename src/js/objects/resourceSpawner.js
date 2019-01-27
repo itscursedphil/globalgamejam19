30ec8c56cef2c31709ef5d263cfa63b26b4408b5
Philip Plagge <sayhi@itscursedphil.com>
2019-01-27 17:13:34 +0100

    Push latest uopdate

--- src/js/objects/resourceSpawner.js
+++ src/js/objects/resourceSpawner.js
@@ -2,29 +2,30 @@ import Event from '../engine/event';
 import Vector2 from '../vector2';
 
 export default class ResourceSpawner {
-  constructor(resource, player) {
+  constructor(resource, player, onSpawn) {
     this.resource = resource;
     this._player = player;
-    this._baseSpawnDelay = 4000;
+    this._onSpawn = onSpawn;
+    this._baseSpawnDelay = 12000;
     this._prevPlayerPos = new Vector2(
       player.position.x + player.rposition.x,
-      player.position.y + player.rposition.x
+      player.position.y + player.rposition.y
     );
 
     this._createSpawnEvent();
   }
 
   _shouldSpawn() {
-    const chance = Math.random() > 0.2;
+    const chance = Math.random() > 0.4;
 
     return chance;
   }
 
   _calcSpawnDelay() {
     // Spawn quicker if resource is low
-    const resourceMod = 1 + this.resource.value * 0.4 - 0.2;
+    const resourceMod = 1 + this.resource.value * 0.2 - 0.4;
     // Random value between 0.8 and 1.2
-    const timeMod = 1 + Math.random() * 0.4 - 0.2;
+    const timeMod = 1 + Math.random() * 0.4 - 0.8;
 
     return this._baseSpawnDelay * resourceMod * timeMod;
   }
@@ -40,24 +41,27 @@ export default class ResourceSpawner {
   }
 
   _spawn() {
-    const dist = window.innerWidth;
+    const dist = 100;
 
-    const playerPosX = this._player.position.x + this._player.rposition.x;
+    const randX = Math.floor(Math.random() * 400) - 800;
+    const randY = Math.floor(Math.random() * 400) - 800;
+
+    const playerPosX = this._player.position.x - this._player.rposition.x;
     const playerPosY = this._player.position.y + this._player.rposition.y;
 
-    const travelX = playerPosX - this._prevPlayerPos.x;
-    const travelY = playerPosY - this._prevPlayerPos.y;
+    const travelX = playerPosX + this._prevPlayerPos.x;
+    const travelY = playerPosY + this._prevPlayerPos.y;
 
-    const posX = playerPosX + travelX * dist;
-    const posY = playerPosY + travelY * dist;
+    const posX = 0 + travelX * -dist;
+    const posY = 0 + travelY * -dist;
 
-    // Console.log(posX, posY);
+    this._onSpawn(posX, posY);
   }
 
   update(tsp) {
     this._spawnEvent.update(tsp);
     this._prevPlayerPos = new Vector2(
-      this._player.position.x + this._player.rposition.x,
+      this._player.position.x - this._player.rposition.x,
       this._player.position.y + this._player.rposition.y
     );
   }
